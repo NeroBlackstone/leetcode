f8ea448ef0a4b8a943b5cfdf5ed98769e339fab3
nero <905374776@qq.com>
2021-01-09 18:41:37 +0800

    26. Remove Duplicates from Sorted Array complete

---
+++ src/removeDuplicatesFromSortedArray/solution.go
@@ -0,0 +1,21 @@
+package removeDuplicatesFromSortedArray
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	amount := 0
+	for _, e := range nums {
+		if e != nums[amount] {
+			amount++
+			nums[amount] = e
+		}
+	}
+	length := amount + 1
+	nums = nums[:length]
+	return length
+}
+
+// Runtime: 12 ms, faster than 26.35% of Go online submissions for Remove Duplicates from Sorted Array.
+//Memory Usage: 4.6 MB, less than 9.56% of Go online submissions for Remove Duplicates from Sorted Array.
