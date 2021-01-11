ee36ed4a66739c12be1bfad65f6ce4ebf73cd561
nero <905374776@qq.com>
2021-01-11 17:35:52 +0800

    38. Count and Say complete

---
+++ src/countAndSay/solution.go
@@ -0,0 +1,41 @@
+package countAndSay
+
+import "strconv"
+
+// https://leetcode.com/problems/count-and-say/
+func countAndSay(n int) string {
+	s := "1"
+	for i := 1; i < n; i++ {
+		s = nextSay(s)
+	}
+	return s
+}
+
+// Get next say
+func nextSay(s string) string {
+	nextStr := ""
+	nowNum := '0'
+	nowCount := 0
+	for _, e := range s {
+		if nowNum != e {
+			if nowNum != '0' {
+				nextStr += getSubStr(nowCount, nowNum)
+			}
+			nowNum = e
+			nowCount = 1
+		} else {
+			nowCount++
+		}
+	}
+	nextStr += getSubStr(nowCount, nowNum)
+	return nextStr
+}
+
+// Get ∣count,digit∣ sub-string
+func getSubStr(count int, num int32) string {
+	return strconv.Itoa(count) + string(num)
+}
+
+// Runtime: 12 ms, faster than 23.08% of Go online submissions for Count and Say.
+//Memory Usage: 7.1 MB, less than 10.44% of Go online submissions for Count and Say.
+// 总结：这是暴力求解，比较慢，但是易于理解。
---
+++ src/countAndSay/solution_test.go
@@ -0,0 +1,36 @@
+package countAndSay
+
+import "testing"
+
+func Test_countAndSay(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Case1",
+			args: args{
+				n: 1,
+			},
+			want: "1",
+		},
+		{
+			name: "Case2",
+			args: args{
+				n: 4,
+			},
+			want: "1211",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countAndSay(tt.args.n); got != tt.want {
+				t.Errorf("countAndSay() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
