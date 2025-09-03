0f9790ce90a455d1d2a58fcfef35f329c7551886
NeroBlackstone <gf7600gs@gmail.com>
2025-09-04 00:23:59 +0800

    add productExceptSelf

---
+++ src/productExceptSelf/index.ts
@@ -0,0 +1,16 @@
+function productExceptSelf(nums: number[]): number[] {
+  const len = nums.length;
+  const arrL = new Array<number>(len);
+  const arrR = new Array<number>(len);
+  arrL[0] = 1;
+  arrR[len - 1] = 1;
+  for (let i = 0; i < len - 1; i++) {
+    arrL[i + 1] = nums[i]! * arrL[i]!;
+    const indexR = len - 1 - i;
+    arrR[indexR - 1] = nums[indexR]! * arrR[indexR]!;
+  }
+  for (let i = 0; i < len; i++) {
+    arrL[i] = arrL[i]! * arrR[i]!;
+  }
+  return arrL;
+}
